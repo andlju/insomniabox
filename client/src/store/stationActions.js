f7167f0a94221de4887da01c7cba20ced0b406f4
Anders Ljusberg <andlju@gmail.com>
2019-10-18 16:15:31 +0200

    Remove old client

--- client/src/store/stationActions.js
+++ client/src/store/stationActions.js
@@ -1,98 +1,104 @@
+import { FETCH_STATION_BEGIN, FETCH_STATION_SUCCESS, FETCH_STATION_FAILURE } from './stationActionTypes';
 
-export const FETCH_STATION_BEGIN = 'FETCH_STATION_BEGIN';
-export const FETCH_STATION_SUCCESS = 'FETCH_STATION_SUCCESS';
-export const FETCH_STATION_FAILURE = 'FETCH_STATION_FAILURE';
+function fetchStationBegin(stationId, name) {
+    return {
+        type: FETCH_STATION_BEGIN,
+        StationId: stationId,
+        StationName: name
+    }
+}
 
-export const fetchStationBegin = (stationId, groupName) => ({
-  type: FETCH_STATION_BEGIN,
-  payload: { StationId: stationId, GroupName: groupName },
-});
+function fetchStationSuccess(stationId, metros) {
+    return { 
+        type: FETCH_STATION_SUCCESS,
+        StationId: stationId,
+        Metros: metros
+    };
+}
 
-export const fetchStationSuccess = (stationId, station) => ({
-  type: FETCH_STATION_SUCCESS,
-  payload: { StationId: stationId, Station: station },
-});
+function fetchStationFailure(stationId, error) {
+    return { 
+        type: FETCH_STATION_FAILURE,
+        StationId: stationId,
+        Error: error
+    };
+}
 
-export const fetchStationFailure = (stationId, error) => ({
-  type: FETCH_STATION_FAILURE,
-  payload: { StationId: stationId, error },
-});
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const USE_FAKE = process.env.REACT_APP_USE_FAKE === 'true';
 
-const API_BASE_URL = process.env.API_BASE_URL;
-const USE_FAKE = process.env.USE_FAKE;
-
-function getStation(stationId, directions) {
+function getStation(stationId) {
   if (USE_FAKE) {
-    return fakeGetStation(stationId, directions);    
+    return fakeGetStation(stationId);    
   }
   return fetch(API_BASE_URL + '/tube/stations/' + stationId + "/realtime").then(resp => resp.json());
 }
 
-function fakeGetStation(stationId, directions) {
-  return new Promise((resolve) => {
-    // Resolve after a timeout so we can see the loading indicator
-    setTimeout(
-      () =>
-        resolve({
-          Station: {
-            StationId: stationId,
-            Metros: [
-              {
-                LineNumber: 13,
-                Destination: 'Ropsten',
-                StopAreaName: 'Axelsberg',
-                DisplayTime: '6 min',
-                JourneyNumber: stationId + 1,
-                JourneyDirection: 1,
-              },
-              {
-                LineNumber: 13,
-                Destination: 'Norsborg',
-                StopAreaName: 'Axelsberg',
-                DisplayTime: '5 min',
-                JourneyNumber: stationId + 2,
-                JourneyDirection: 2,
-              },
-              {
-                LineNumber: 13,
-                Destination: 'Ropsten',
-                StopAreaName: 'Axelsberg',
-                DisplayTime: '12 min',
-                JourneyNumber: stationId + 3,
-                JourneyDirection: 1,
-              },
-              {
-                LineNumber: 13,
-                Destination: 'Alby',
-                StopAreaName: 'Axelsberg',
-                DisplayTime: '15 min',
-                JourneyNumber: stationId + 4,
-                JourneyDirection: 2,
-              },
-            ],
-          }
-        }),
-      Math.random() * 2000,
-    );
-  });
-}
-
-export function fetchStation(stationId, groupName, stationName, directions) {
-  return (dispatch) => {
-    // Set initial state
-    dispatch(fetchStationBegin(stationId, groupName));
-    // Kick off the backend call
-    return getStation(stationId, directions)
-      .then((json) => {
-        json.Station.StationId = stationId;
-        json.Station.StationName = stationName;
-        json.Station.Metros = directions.map(d => json.Station.Metros.filter(m => m.JourneyDirection === d)).flat();
-                
-        dispatch(fetchStationSuccess(stationId, json.Station));
-        return json;
-      })
-      .catch(error =>
-        dispatch(fetchStationFailure(stationId, error))
+function fakeGetStation(stationId) {
+    return new Promise((resolve) => {
+      // Resolve after a timeout so we can see the loading indicator
+      setTimeout(
+        () =>
+          resolve({
+            Station: {
+              StationId: stationId,
+              Metros: [
+                {
+                  LineNumber: 13,
+                  Destination: 'Ropsten',
+                  StopAreaName: 'Axelsberg',
+                  DisplayTime: '6 min',
+                  JourneyNumber: stationId + 1,
+                  JourneyDirection: 1,
+                },
+                {
+                  LineNumber: 13,
+                  Destination: 'Norsborg',
+                  StopAreaName: 'Axelsberg',
+                  DisplayTime: '5 min',
+                  JourneyNumber: stationId + 2,
+                  JourneyDirection: 2,
+                  Deviations: [{Text: "Inställd", Consequence: "CANCELLED", ImportanceLevel: 0}]
+                },
+                {
+                  LineNumber: 13,
+                  Destination: 'Ropsten',
+                  StopAreaName: 'Axelsberg',
+                  DisplayTime: '12 min',
+                  JourneyNumber: stationId + 3,
+                  JourneyDirection: 1,
+                },
+                {
+                  LineNumber: 13,
+                  Destination: 'Alby',
+                  StopAreaName: 'Axelsberg',
+                  DisplayTime: '15 min',
+                  JourneyNumber: stationId + 4,
+                  JourneyDirection: 2,
+                },
+              ],
+            }
+          }),
+        Math.random() * 2000,
       );
-  };
-}
+    });
+  }
+  
+
+export function fetchStation(stationId, stationName) {
+    return (dispatch) => {
+      // Set initial state
+      dispatch(fetchStationBegin(stationId, stationName));
+      // Kick off the backend call
+      return getStation(stationId)
+        .then((json) => {
+            const metros = json.Station.Metros;
+            dispatch(fetchStationSuccess(stationId, metros));
+          return json;
+        })
+        .catch(error =>
+          dispatch(fetchStationFailure(stationId, error))
+        );
+    };
+  }
+  
